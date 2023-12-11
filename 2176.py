1ff43546ceaf3e5bb03c5ab5bcb3a459bacac4bb
Johnson Joseph <117355554+Johnsonj302@users.noreply.github.com>
2023-12-11 10:39:49 +0530

    Add files via upload
    2176---->Count Equal and Divisible Pairs in an Array
    
    Given a 0-indexed integer array nums of length n and an integer k, return the number of pairs (i, j) where 0 <= i < j < n, such that nums[i] == nums[j] and (i * j) is divisible by k.

---
+++ 2176.py
@@ -0,0 +1,15 @@
+"""Count Equal and Divisible Pairs in an Array
+
+Given a 0-indexed integer array nums of length n and an integer k,
+ return the number of pairs (i, j) where 0 <= i < j < n,
+   such that nums[i] == nums[j] and (i * j) is divisible by k.
+"""
+
+class Solution:
+    def countPairs(self, nums, k):
+        count = 0
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] == nums[j] and (i*j)%k == 0:
+                    count +=1
+        return count
